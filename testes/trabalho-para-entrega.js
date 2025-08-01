dbd5f20345856618272d2e93f04f111e134537b9
Fabio Quermes <fabiomobi@gmail.com>
2025-08-01 18:04:37 -0300

    Atualiza export

--- testes/trabalho-para-entrega.js
+++ testes/trabalho-para-entrega.js
@@ -7,20 +7,20 @@ export {
   buscarDadoAsync
 };
 
-// 1. Gera tag de identificação em maiúsculas (sem return)
+// 1. Gera tag de identificação em maiúsculas 
 const geradorDeTagsDeIdentificacao = nome => nome.toUpperCase();
 
-// 2. Verifica se pode ser adotado: idade = 1 e porte = 'M' (sem return)
+// 2. Verifica se pode ser adotado: idade = 1 e porte = 'M' 
 const verificarSePodeSerAdotado = (idade, porte) => idade === 1 && porte === 'M';
 
-// 3. Calcula consumo de ração: peso * 300 (sem return)
+// 3. Calcula consumo de ração: peso * 300 
 const calcularConsumoDeRacao = (nome, idade, peso) => peso * 300;
 
-// 4. Decide tipo de atividade por porte (sem return)
+// 4. Decide tipo de atividade por porte 
 const decidirTipoDeAtividadePorPorte = porte => 
   (porte === 'pequeno' && 'brincar dentro de casa') || '';
 
-// 5. Busca dado de forma assíncrona (sem return)
+// 5. Busca dado de forma assíncrona 
 const buscarDadoAsync = async () => new Promise(resolve => setTimeout(() => resolve('Pipoca'), 10));
 
 
