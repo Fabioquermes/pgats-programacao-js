a3b28973cc80434a5b95ad6f5fed3c6831a4562a
Fabio Quermes <fabiomobi@gmail.com>
2025-08-01 16:31:56 -0300

    async-await

---
+++ conceitos/async-await.js
@@ -0,0 +1,10 @@
+
+
+// async - assincrono
+async function exibirNomeDogFormatado (nome) {
+    return nome.toUpperCase()
+    
+}
+
+// await - esperar
+console.log(await exibirNomeDogFormatado(`Pipoca`))
